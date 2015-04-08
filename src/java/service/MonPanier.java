df1413d314003913175e5a7ed3090c155cc323a6
Moi <Moi@172.29.82.188>
2015-04-08 18:16:47 -0400

    Leila: changement dans monpanier.java

--- src/java/service/MonPanier.java
+++ src/java/service/MonPanier.java
@@ -12,19 +12,17 @@ import java.text.DecimalFormat;
 import java.util.Map;
 
 public class MonPanier {
-    
+
     //float montantTotal;
     Map<String, Produits> hm = new HashMap<String, Produits>();
 
     public Produits getHm(String id) {
         return hm.get(id);
     }
-    
-    
 
     //ajoute produit dans mon cart
     public void ajouterProduit(String id, int nombreProduit) {
-        System.out.println("fonc : ajouterProduit : id = "+id);
+        System.out.println("fonc : ajouterProduit : id = " + id);
         Produits produitCart = new Produits();
         if (hm.containsKey(id)) {
             produitCart = hm.get(id);
@@ -35,22 +33,19 @@ public class MonPanier {
             produitCart.setShoppingNum(nombreProduit);
             hm.put(id, produitCart);
         }
-         System.out.println("fonc : ajouterProduit : produit = "+hm.get(id).getDescription());
+        System.out.println("fonc : ajouterProduit : produit = " + hm.get(id).getDescription());
 
     }
-
 
     public void supprimerProduit(String id) {
         hm.remove(id);
     }
-
 
     public void modifierProduit(String id, int nums) {
 
         Produits book = hm.get(id);
         book.setShoppingNum(nums);
     }
-
 
     public ArrayList afficherMonPanier() {
         ArrayList al = new ArrayList();
@@ -74,24 +69,25 @@ public class MonPanier {
         while (iterator.hasNext()) {
 
             String noProduit = (String) iterator.next();
-            System.out.println("MonPanier.java------ getMmontantTotal()-----noProduit :"+noProduit);
+            System.out.println("MonPanier.java------ getMmontantTotal()-----noProduit :" + noProduit);
 
             Produits produit = hm.get(noProduit);
 
             toltalPrice += produit.getPrix() * produit.getShoppingNum();
         }
-        DecimalFormat df = new DecimalFormat("#.00");
-        toltalPrice = Float.parseFloat(df.format(toltalPrice));
+        if (toltalPrice != 0) {
+            DecimalFormat df = new DecimalFormat("#.00");
+            toltalPrice = Float.parseFloat(df.format(toltalPrice));
+        }
         return toltalPrice;
     }
-
 
     public void viderMonPanier() {
         hm.clear();
     }
-    
-    public int quantiteProduit(){
-        return(hm.size());
+
+    public int quantiteProduit() {
+        return (hm.size());
     }
 
 }
